285076ca06b91e3191327447f5199e14518443ec
wtni-gidle <81308718+wtni-gidle@users.noreply.github.com>
2026-03-26 19:21:46 +0800

    优化sup_template.py，支持处理.cif文件，改进路径处理和输出文件管理

--- PDBToolkit/CASP/sup_template.py
+++ PDBToolkit/CASP/sup_template.py
@@ -8,6 +8,7 @@ import pandas as pd
 import logging
 from multiprocessing import Pool
 import argparse
+from pathlib import Path
 
 from PDBToolkit.config import USALIGN_PATH
 
@@ -46,7 +47,7 @@ def wrapper(model, reference, output_prefix, extra_args):
 
 
 def process_in_parallel(model_dir, reference_file, sup_dir = None, extra_args = None, n_cpu = 1):
-    model_list = [os.path.join(model_dir, model) for model in os.listdir(model_dir) if model.endswith('.pdb')]
+    model_list = [os.path.join(model_dir, model) for model in os.listdir(model_dir) if model.endswith('.pdb') or model.endswith('.cif')]
     reference_list = [reference_file for model in model_list]
     if sup_dir:
         output_prefix_list = [os.path.join(sup_dir, os.path.basename(model).replace(".pdb", "_sup")) for model in model_list]
@@ -64,29 +65,34 @@ def process_in_parallel(model_dir, reference_file, sup_dir = None, extra_args = 
 
 
 def main(args):
-    model_dir = os.path.abspath(args.model_dir)
-    reference_file = os.path.abspath(args.reference)
+    model_dir = Path(args.model_dir)
+    reference_file = Path(args.reference)
     if args.sup_dir:
-        sup_dir = os.path.abspath(args.sup_dir)
-        os.makedirs(sup_dir, exist_ok=True)
+        sup_dir = Path(args.sup_dir)
+        sup_dir.mkdir(parents=True, exist_ok=True)
     else:
         sup_dir = None
-    tmscore_dict = process_in_parallel(model_dir, reference_file, sup_dir, args.extra_args, args.n_cpu)
+
+    tmscore_dict = process_in_parallel(
+        model_dir.resolve().as_posix(), 
+        reference_file.resolve().as_posix(), 
+        sup_dir.resolve().as_posix(), 
+        args.extra_args.split(" ") if args.extra_args else None, 
+        args.n_cpu
+    )
     tmscore_dict = {os.path.basename(k): v for k, v in tmscore_dict.items()}
     tmscore_df = pd.DataFrame({"model": tmscore_dict.keys(), "tmscore": tmscore_dict.values()})
 
     if args.output_file is not None:
-        output_path = os.path.abspath(args.output_file)
+        output_path = Path(args.output_file)
     else:
-        output_path = os.path.join(sup_dir, "tmscore.csv")
-    tmscore_df.to_csv(output_path, index=False, sep="\t")
-    
-    for filename in os.listdir(sup_dir):
-        if filename.endswith(".pml"):
-            file_path = os.path.join(sup_dir, filename)
-            os.remove(file_path)
-
+        output_path = sup_dir / "tmscore.csv"
+    tmscore_df.to_csv(output_path, index=False)
     logging.info(f"Results saved to {output_path}")
+    
+    if sup_dir:
+        for file in sup_dir.glob("*.pml"):
+            os.remove(file)
     
 
 if __name__ == "__main__":
@@ -95,7 +101,7 @@ if __name__ == "__main__":
     parser.add_argument("reference", help="Reference PDB file.")
     parser.add_argument("--sup_dir", help="Directory to save the output PDB files.")
     parser.add_argument("--output_file", help="Output file to save the TM-score results.")
-    parser.add_argument('--extra_args', nargs='*', default=None, 
+    parser.add_argument('--extra_args', type=str, default=None, 
                         help='Additional arguments for USalign.')
     parser.add_argument("--n_cpu", type=int, default=1, help="Number of CPUs to use.")
     args = parser.parse_args()
